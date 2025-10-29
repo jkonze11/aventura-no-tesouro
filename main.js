5becbf8cb4729092ce1710d28c2698b84b633d81
jkonze11 <pedro.silva.pontes@escola.pr.gov.br>
2025-10-29 07:58:46 -0300

    wev

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+const avanca = document.querySelectorAll ('.btn-proximo')
+    console. log (avanca)
+
+avanca.forEach(button =>{ 
+    button.addEventListener ('click ',fuunction(){
+        const atual =document.querySelector('.ativo')
+        const proximoPasso ='passo'+ getAttribute ('data')
+
+    })
 
 
-
-
+})
 
 
 
